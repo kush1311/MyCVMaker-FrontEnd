819ef33fc4924bfa1b79525d5d430627a499bebb
Het <hetdesai068@gmail.com>
2023-05-11 11:54:47 +0530

    Removed reportWebVitals

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
-import reportWebVitals from './reportWebVitals';
 import GlobalContextApi from './components/GlobalContextApi/GlobalContextApi';
 
 ReactDOM.render(
@@ -17,4 +16,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
