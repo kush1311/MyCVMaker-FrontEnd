b7c78cf43b9900f2bbf686a63b2453846d6ac18e
Het <hetdesai068@gmail.com>
2023-05-11 17:30:23 +0530

    Added another layer of base64 after encryption to avoid special character, especially "/". Because it messes with the request URL when used as params

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,13 +1,18 @@
 const CryptoJS = require('crypto-js');
 
-const PRIVATE_KEY = process.env.CRYPTO_PRIVATE_KEY;
+console.log(process.env);
+
+const PRIVATE_KEY = process.env.REACT_APP_CRYPTO_PRIVATE_KEY;
+console.log('PRIVATE_KEY --> ', PRIVATE_KEY);
 
 function encrypt(text) {
-  return CryptoJS.AES.encrypt(text, PRIVATE_KEY).toString();
+  const encrypted = CryptoJS.AES.encrypt(text, PRIVATE_KEY).toString();
+  return Buffer.from(encrypted).toString('base64');
 }
 
 function decrypt(encryptedData) {
-  const bytes = CryptoJS.AES.decrypt(encryptedData, PRIVATE_KEY);
+  const base64DecodedString = Buffer.from(encryptedData, 'base64').toString('utf-8');
+  const bytes = CryptoJS.AES.decrypt(base64DecodedString, PRIVATE_KEY);
   return bytes.toString(CryptoJS.enc.Utf8);
 }
 
