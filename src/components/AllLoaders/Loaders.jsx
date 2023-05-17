f602ad00384b5facbb408e17c2f96d20c4900064
Het <hetdesai068@gmail.com>
2023-05-17 21:33:00 +0530

    Created full page loader

--- src/components/AllLoaders/Loaders.jsx
+++ src/components/AllLoaders/Loaders.jsx
@@ -2,15 +2,23 @@ import React from "react";
 
 export const Loader = (props) => {
   const { color, size } = props;
-  const lcolor = color ? color : "text-white";
-  const lsize = size ? size : "spinner-border-white";
+  const loaderColor = color ? color : "text-white";
+  const loaderSize = size ? size : "1";
   return (
     <div
+    style={{width: `${loaderSize}rem`, height: `${loaderSize}rem`}}
       className={`spinner-border spinner-border-sm text-black ${
-        lcolor + " " + lsize
+        loaderColor + " " + loaderSize
       }`}
       role='status'>
       <span className='sr-only'>Loading...</span>
     </div>
   );
 };
+
+export const FullPageLoader = () => {
+  return (
+    <div className="h-screen flex"><Loader color='text-sky-600 mx-auto my-auto' size='5' /></div>
+  )
+}
+
