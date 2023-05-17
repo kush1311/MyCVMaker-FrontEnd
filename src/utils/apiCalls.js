74216d78bddbf2da3a694f59ab7329a6e4fb3dcb
Het <hetdesai068@gmail.com>
2023-05-17 12:42:31 +0530

    Added logout, saveResumeData and getResumeTemplateData handling

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { encrypt, decrypt } from "./crypto";
+import logController from "./logController";
+
+logController()
 
 axios.defaults.withCredentials = true
-
 const SERVER_BASE_URL = process.env.REACT_APP_SERVER_BASE_URL;
 console.log('SERVER_BASE_URL -->', SERVER_BASE_URL);
-console.log(process.env);
+
 const API_URL = {
     REGISTER: `${SERVER_BASE_URL}/user/register`,
     LOGIN: `${SERVER_BASE_URL}/user/login`,
@@ -37,15 +39,16 @@ const registerApiCall = async ({ email, password, firstName, lastName }) => {
 
 const loginApiCall = async ({ email, password }) => {
         try {
+            console.log(`${API_URL.LOGIN}/${email}/${password}`);
             const result = await axios.get(`${API_URL.LOGIN}/${encrypt(email)}/${encrypt(password)}`);
             const {userData} = result.data;
-            const userId = decrypt(userData.userId);
+            const userId = JSON.parse(decrypt(userData.userId));
             const firstName = decrypt(userData.firstName);
             const lastName = decrypt(userData.lastName);
             const resumeIdArray = userData.resumeIdArray;
             for (let index = 0; index < resumeIdArray.length; index++) {
                 const resumeId = resumeIdArray[index];
-                resumeIdArray[index] = decrypt(resumeId);
+                resumeIdArray[index] = JSON.parse(decrypt(resumeId));
             }
             return {userId, firstName, lastName, resumeIdArray, status: result.status};
         } catch (error) {
@@ -57,8 +60,12 @@ const loginApiCall = async ({ email, password }) => {
 const verifyTokenApiCall = async () => {
     try {
         const result = await axios.get(API_URL.VERIFY_TOKEN);
-        console.log(result);
-        return result;
+        const { status, data } = result;
+        const decryptedResult = {};
+        decryptedResult.status = status;
+        decryptedResult.resumeIdArray = data.resumeIdArray.map((resumeId) => decrypt(resumeId))
+        decryptedResult.userId = decrypt(data.userId);
+        return decryptedResult;
     } catch (error) {
         console.log(error);
         console.log(error.response)
@@ -68,104 +75,77 @@ const verifyTokenApiCall = async () => {
 
 const createResumeApiCall = async (userId) => {
     try {
+        if (!userId) {
+            alert(`${userId}`);
+        }
         const result = await axios.post(`${API_URL.USER_BASE_URL}/${encrypt(userId)}/resume/`)
-        return result;
+        const newResumeIdArray = [];
+        for (let index = 0; index < result.data.resumeIdArray.length; index++) {
+            const resumeId = decrypt(result.data.resumeIdArray[index]);
+            newResumeIdArray.push(resumeId);
+        }
+        return newResumeIdArray;
     } catch (error) {
         console.log(error);
         throw error;
     }
 }
-const getResume = async () => {
-    const userId = localStorage.getItem("%su!I#d");
 
-    if (!userId) {
-        return message.logout
+const getResumeData = async (userId, resumeId) => {
+    try {
+        if (!userId || !resumeId) {
+            alert(`${userId} ${resumeId}`);
+        }
+        const result = await axios.get(`${API_URL.USER_BASE_URL}/${encrypt(userId)}/resume/${encrypt(resumeId)}`)
+        // Object containing all CV state data as properties
+        return result;
+
+    } catch (err) {
+        console.log(err);
+        throw err;
     }
-    return await axios
-        .get(`${process.env.REACT_APP_URL}/resume/${userId}`)
-        .then((res) => {
-            //console.log(res)
-            if (res && res.data && res.data && res.status && res.status === 200) {
-                // localStorage.setItem("%ru!I#d", res.data.id);
-            }
-        })
-        .catch((err) => {
-            if (err && err.response && err.response.status) {
-                const status = err.response.status
-                if (status === 500) {
-                    return message.server_error
-                }
-                else if (status === 401) {
-                    return message.logout
-                }
-            }
-        });
 }
-const getResumeData = async () => {
-    const userId = localStorage.getItem("%su!I#d");
-    const resumeId = localStorage.getItem("%ru!I#d");
 
-    return await axios
-        .get(`${API_URL.USER_BASE_URL}/${encrypt(userId)}/resume/${encrypt(resumeId)}`)
-        .then((res) => {
-            //console.log(res.data[0])
-            if (res && res.data && res.data && res.status && res.status === 200) {
-                // Object containing all CV state data as properties
-                return res.data[0]
-            }
-        })
-        .catch((err) => {
-            if (err && err.response && err.response.status) {
-                const status = err.response.status
-                if (status === 500) {
-                    return message.server_error
-                }
-                else if (status === 401) {
-                    return message.logout
-                } else if (status === 404) {
-                    return message.server_error
-                }
-            }
+const getResumeTemplateData = async () => {
+    try {
+        const result = await axios.get(`${API_URL.USER_BASE_URL}/resume/template`)
+        return result;
 
-
-        });
-}
-const logout_Api_Call = async () => {
-    const userId = localStorage.getItem("%su!I#d");
-
-    if (!userId) {
-        return message.logout
+    } catch (err) {
+        console.log(err);
+        throw err;
     }
-    return await axios
-        .get(`${process.env.REACT_APP_URL}/logout}`)
-        .then((res) => {
-            //console.log(res)
-            if (res && res.status && res.status === 200) {
-                return res.status
-            }
+}
+
+const saveResumeData = async (userId, resumeId, body) => {
+    try {
+        if (!userId || !resumeId) {
+            alert(`${userId} ${resumeId}`);
+        }
+        const result = await axios.put(`${API_URL.USER_BASE_URL}/${encrypt(userId)}/resume/${encrypt(resumeId)}`,{
+            ...body
         })
-        .catch((err) => {
-            if (err && err.response && err.response.status) {
-                const status = err.response.status
-                if (status === 500) {
-                    return message.server_error
-                }
-                else if (status === 401) {
-                    return message.logout
-                }
-            }
+        // Object containing all CV state data as properties
+        return result;
 
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
 
-        });
+const logoutApiCall = async () => {
+    return await axios.get(`${API_URL.USER_BASE_URL}/logout`)
 }
 
 export {
     message,
     createResumeApiCall,
-    getResume,
     getResumeData,
-    logout_Api_Call,
+    logoutApiCall,
     registerApiCall,
     loginApiCall,
     verifyTokenApiCall,
+    saveResumeData,
+    getResumeTemplateData,
 }
